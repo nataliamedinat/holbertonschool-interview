9a00a7e83f46f1bfd2f363d54c872fcc8d4305a4
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-21 19:54:30 -0500

    Lockboxes problem

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Solution of the pronlem"""
+
+def canUnlockAll(boxes):
+    """ Prototype """
+
+    key = [0]
+
+    for trav in key:
+        for i in boxes[trav]:
+            if (i not in key and i < len(boxes):
+                key.Append (i)
+
+    if (len(key) == len (boxes)):
+        return True
+    else:
+        return False
