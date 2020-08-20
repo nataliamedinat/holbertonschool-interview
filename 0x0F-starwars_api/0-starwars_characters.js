42e2cf18c7730223d0662516b6738d2f9947177f
nataliamedinat <nataliamedinabotero@gmail.com>
2020-08-20 02:47:26 +0000

    permissions

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,31 +1,21 @@
 #!/usr/bin/node
-if (process.argv.length !== 3) {
-    console.log('Usage: ./0-starwars_characters.js <film number>');
+
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
-    getCharactersInOrder(process.argv[2], 'https://swapi.co/api/people/');
+    const all = JSON.parse(body).characters;
+    for (const cha of all) {
+      request(cha, function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
-  
-  function getCharactersInOrder (filmID, chURL) {
-    const request = require('request');
-    request(chURL, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      } else {
-        const json = JSON.parse(body);
-        chURL = json.next;
-        const characters = json.results;
-        characters.forEach(function (character) {
-          const films = character.films;
-          films.forEach(function (film) {
-            if (film.includes(filmID)) {
-              console.log(character.name);
-            }
-          });
-        });
-      }
-      if (chURL !== null) {
-        getCharactersInOrder(filmID, chURL);
-      }
-    });
-  }
-  
+});
