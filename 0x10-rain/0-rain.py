37d3ae12003ebbe99cf5e19c7d9aa123f96446e0
nataliamedinat <nataliamedinabotero@gmail.com>
2020-09-03 04:00:41 +0000

    Final return

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""0_rain"""
+""" rain"""
 
 def rain(walls):
-    """rain function"""
-    if type(walls) is not list:
-        return 0
-    if len(walls) is 0:
-        return 0
-    # To store the maximum water 
-    # that can be stored 
+    """
+    Given a list of non-negative integers representing walls of width 1, calcula    te how much water will be retained after it rains.
+    """
+    length = len(walls)
     res = 0
-    n = len(walls)
-     
-    # For every element of the array 
-    for i in range(1, n - 1) : 
-         
-        # Find the maximum element on its left 
-        left = arr[i]; 
-        for j in range(i) :
-            left = max(left, arr[j]); 
-         
-        # Find the maximum element on its right 
-        right = arr[i]; 
-         
-        for j in range(i + 1 , n) : 
-            right = max(right, arr[j]);
-             
-        # Update the maximum water
-        res = res + (min(left, right) - arr[i]); 
- 
-    return res; 
+
+    for i in range(1, length - 1):
+
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        right = walls[i]
+
+        for j in range(i + 1, length):
+            right = max(right, walls[j])
+
+        res = res + (min(left, right) - walls[i])
+    return res
