87d56ef5a46112985d443080a9fa04b271d16eae
nataliamedinat <nataliamedinabotero@gmail.com>
2020-09-03 00:00:21 +0000

    Heap Sort

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,14 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+
+/* 8. Heap sort */
+void heap_sort(int *array, size_t size);
+
+
+#endif
